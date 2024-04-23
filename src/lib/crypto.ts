35a0d8de1fd48249db8dee5eee555b02a268d0c2
wyattlake <lakewyatt@gmail.com>
2024-04-23 00:13:17 -0700

    Added decryption + public and private key generation

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -5,12 +5,27 @@ export class ECPoint {
 	) {}
 }
 
+export class EncryptedMessage {
+	constructor(
+		public x: ECPoint,
+		public y: ECPoint
+	) {}
+}
+
 export class Curve {
 	constructor(
 		public A: bigint,
 		public B: bigint,
 		public Fp: bigint
 	) {}
+
+	static alt_bn128(): Curve {
+		return new Curve(
+			BigInt(0),
+			BigInt(3),
+			BigInt('21888242871839275222246405745257275088696311157297823662689037894645226208583')
+		);
+	}
 
 	positiveMod(a: bigint, n: bigint) {
 		const initialResult = a % n;
@@ -127,5 +142,81 @@ export class Curve {
 		} else {
 			return this.pAdd(this.pMul(this.pDouble(p), n / BigInt(2)), p);
 		}
+	}
+
+	private mask(arr: Uint8Array, numBits: number) {
+		let idx = 0;
+		let b = numBits;
+		while (idx < arr.length) {
+			arr[idx] = b <= 0 ? 0 : b > 0 && b < 8 ? arr[idx] & ((1 >> b) - 1) : arr[idx];
+			++idx;
+			b -= 8;
+		}
+	}
+
+	private getBitCount(v: bigint) {
+		let x = v;
+		let c = 0;
+		do {
+			++c;
+			x >>= 1n;
+		} while (x > 0n);
+		return c;
+	}
+
+	// Thanks stackoverflow
+	private bigRandomInRange(v1: bigint, v2: bigint) {
+		const min = v1 > v2 ? v2 : v1;
+		const r = v1 - v2;
+		const range = r < 0 ? -r : r;
+		if (range === 0n) {
+			return min;
+		}
+		const bitCount = this.getBitCount(range);
+		const byteCount = Math.ceil(bitCount / 8);
+		const arr = new Uint8Array(byteCount);
+		for (;;) {
+			crypto.getRandomValues(arr);
+			this.mask(arr, bitCount);
+			const v = arr.reduce((acc, curr, i) => (1n << (BigInt(i) * 8n)) * BigInt(curr) + acc, 0n);
+			if (v < range) {
+				return min + v;
+			}
+		}
+	}
+
+	public generatePrivateKey() {
+		return this.bigRandomInRange(
+			BigInt(0),
+			BigInt('115792089237316195423570985008687907853269984665640564039457584007913129639936') // 2^256
+		);
+	}
+
+	public generatePublicKey(privateKey: bigint): ECPoint | undefined {
+		return this.pMul(new ECPoint(BigInt(1), BigInt(2)), privateKey);
+	}
+
+	private decodeRawPoint(p: ECPoint): bigint {
+		return p.x >> BigInt(10);
+	}
+
+	public pNeg(p: ECPoint | undefined): ECPoint | undefined {
+		if (p == undefined) {
+			return undefined;
+		} else if (p.x == BigInt(0) && p.y == BigInt(0)) {
+			return new ECPoint(BigInt(0), BigInt(0));
+		} else {
+			return new ECPoint(p.x, this.positiveMod(-p.y, this.Fp));
+		}
+	}
+
+	public decrypt(privateKey: bigint, message: EncryptedMessage): bigint {
+		const decryptedPoint = this.pAdd(message.y, this.pNeg(this.pMul(message.x, privateKey)));
+
+		if (decryptedPoint == undefined) {
+			return BigInt(0);
+		}
+
+		return this.decodeRawPoint(decryptedPoint);
 	}
 }
--- src/routes/cryptotest/+page.svelte
+++ src/routes/cryptotest/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
-	import { Curve, ECPoint } from '$lib/crypto';
+	import { Curve, ECPoint, EncryptedMessage } from '$lib/crypto';
 
-	const alt_bn128 = new Curve(
-		BigInt(0),
-		BigInt(3),
-		BigInt('21888242871839275222246405745257275088696311157297823662689037894645226208583')
+	const ecc = Curve.alt_bn128();
+
+	// const privateKey = ecc.generatePrivateKey();
+	const privateKey = BigInt(12);
+	// const publicKey = ecc.generatePublicKey(privateKey);
+
+	console.log(
+		ecc.decrypt(
+			privateKey,
+			new EncryptedMessage(
+				new ECPoint(
+					BigInt('6880448689389350391313156031345092332727233274631084306710044265222472228983'),
+					BigInt('14628804599421596126450424756703084123588574405738088923158300616205465232619')
+				),
+				new ECPoint(
+					BigInt('20315032863293399502308717084041282382392882649452966993642829822642984509024'),
+					BigInt('5908621500160417034324881136872653040519357755556178648959151499352524029854')
+				)
+			)
+		)
 	);
-
-	const gen_point = new ECPoint(BigInt(1), BigInt(2));
-
-	const x = BigInt('238432489239');
-	console.log(alt_bn128.pMul(gen_point, x));
 </script>
